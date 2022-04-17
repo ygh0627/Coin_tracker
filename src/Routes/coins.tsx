d2fd5e6b08a7a6c1ac7eaaec729f7f4c48bf5aa6
ygh0627 <ygh970627@gmail.com>
2022-04-17 13:33:18 +0900

    messagee

--- src/Routes/coins.tsx
+++ src/Routes/coins.tsx
@@ -7,7 +7,6 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { useSetRecoilState } from "recoil";
 import { isDarkAtom } from "../atoms";
 import { motion, AnimatePresence } from "framer-motion";
-import TypeIt from "typeit-react";
 
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
